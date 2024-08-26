ea12d47db7c51ae3b233d8dd707e89c55882d3cd
Faradaii <kdkfrdy@gmail.com>
2024-08-26 12:34:22 +0800

    init: initialized repository

---
+++ README.md
@@ -0,0 +1,4 @@
+# HEFI (Hackathon Event Finder)
+
+A Flutter application that provide developers or public to find hackathons.
+
---
+++ android/app/src/main/kotlin/com/example/hefi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hefi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
